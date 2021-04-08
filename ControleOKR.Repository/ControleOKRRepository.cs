e62c12770ebacd6f8c166da0c0dbe60de97afe73
rafaelmegda <rafaelmisidoro@gmail.com>
2021-04-07 21:05:08 -0300

    conclusão metodos add. put, delete and save

--- ControleOKR.Repository/ControleOKRRepository.cs
+++ ControleOKR.Repository/ControleOKRRepository.cs
@@ -5,16 +5,36 @@ namespace ControleOKR.Repository
 {
     public class ControleOKRRepository : IControleOKRRepository
     {
+        private readonly ControleOKRContext _context;
+
+        //Construtor do contexto do banco de dados
+        public ControleOKRRepository(ControleOKRContext context)
+        {
+            _context = context;
+        }
+
+        //GERAIS
         public void Add<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _context.Add(entity);
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Update(entity);
         }
 
         public void Delete<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _context.Remove(entity);
         }
 
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+
+        //EMPRESAS
         public Task<Empresa[]> GetAllEmpresaAsyncByNome(string nome, bool inluirObjetivos)
         {
             throw new System.NotImplementedException();
@@ -24,7 +44,12 @@ namespace ControleOKR.Repository
         {
             throw new System.NotImplementedException();
         }
+        public Task<Empresa> GetEmpresaAsyncById(int EmpresaId, bool incluirObjetivos)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        //OBJETIVOS
         public Task<Objetivo[]> GetAllObjetivosAsync(bool inluirResultadosChaves)
         {
             throw new System.NotImplementedException();
@@ -35,6 +60,12 @@ namespace ControleOKR.Repository
             throw new System.NotImplementedException();
         }
 
+        public Task<Objetivo> GetObjetivoAsyncById(int ObjetivoId, bool inluirResultadosChaves)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        //RESULTADO CHAVE ATIVIDADE
         public Task<ResultadoChaveAtividade[]> GetAllResultadosChaveAtividadesAsync()
         {
             throw new System.NotImplementedException();
@@ -44,7 +75,12 @@ namespace ControleOKR.Repository
         {
             throw new System.NotImplementedException();
         }
+        public Task<ResultadoChaveAtividade> GetResultadoChaveAtividadeAsyncById(int ResultadoChaveAtividadeId)
+        {
+            throw new System.NotImplementedException();
+        }
 
+        //RESULTADO CHAVE
         public Task<ResultadoChave[]> GetAllResultadosChavesAsync(bool inluirAtividadesResultadosChaves)
         {
             throw new System.NotImplementedException();
@@ -55,32 +91,7 @@ namespace ControleOKR.Repository
             throw new System.NotImplementedException();
         }
 
-        public Task<Empresa> GetEmpresaAsyncById(int EmpresaId, bool incluirObjetivos)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<Objetivo> GetObjetivoAsyncById(int ObjetivoId, bool inluirResultadosChaves)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public Task<ResultadoChave> GetResultadoChaveAsyncById(int ResultadoChaveId, bool inluirAtividadesResultadosChaves)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<ResultadoChaveAtividade> GetResultadoChaveAtividadeAsyncById(int ResultadoChaveAtividadeId)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<bool> SaveChangesAsync()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Update<T>(T entity) where T : class
         {
             throw new System.NotImplementedException();
         }
